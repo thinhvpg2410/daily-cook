d422b9c74c4de8e5d187f11b44b032982274299a
triet03 <127522140+triet03@users.noreply.github.com>
2025-09-11 09:25:38 +0700

    Delete backend directory

--- backend/src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { FirebaseAdminProvider } from '../auth/firebase-admin.provider';
-
-@Module({
-  providers: [UsersService, FirebaseAdminProvider],
-  exports: [UsersService],
-})
-export class UsersModule {}
