798f695bdc1ee992854a0d4877d9288b89b01bdc
triet03 <127522140+triet03@users.noreply.github.com>
2025-09-17 15:51:40 +0700

    Update App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// Screens
 import LaunchScreen from "./src/screens/LaunchScreen";
 import OnboardingScreen from "./src/screens/OnboardingScreen";
 import SignInEmail from "./src/screens/SignInEmail";
@@ -10,28 +9,31 @@ import SignUpEmail from "./src/screens/SignUpEmail";
 import ForgotPasswordEmail from "./src/screens/ForgotPasswordEmail";
 import ForgotPasswordCode from "./src/screens/ForgotPasswordCode";
 import ResetPassword from "./src/screens/ResetPassword";
+import HomeScreen from "./src/screens/HomeScreen";
+import CategoryScreen from "./src/screens/CategoryScreen";
+import DetailsScreen from "./src/screens/DetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Launch"
-        screenOptions={{ headerShown: false }}
-      >
+      <Stack.Navigator initialRouteName="Launch" screenOptions={{ headerShown: false }}>
         {/* Launch & Onboarding */}
         <Stack.Screen name="Launch" component={LaunchScreen} />
         <Stack.Screen name="Onboarding" component={OnboardingScreen} />
 
-        {/* Authentication */}
+        {/* Auth */}
         <Stack.Screen name="SignInEmail" component={SignInEmail} />
         <Stack.Screen name="SignUpEmail" component={SignUpEmail} />
-
-        {/* Forgot Password Flow */}
         <Stack.Screen name="ForgotPasswordEmail" component={ForgotPasswordEmail} />
         <Stack.Screen name="ForgotPasswordCode" component={ForgotPasswordCode} />
         <Stack.Screen name="ResetPassword" component={ResetPassword} />
+
+        {/* Main */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Category" component={CategoryScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
